16b2ff0af39efaeb5de86e9324b93a1bab126666
Nico Villar <nvillar@marppa.com>
2023-12-24 17:51:09 -0300

    [#5] Add year selector card and don't use state for the progress bar date

--- mobile/lib/screens/home/home_screen.dart
+++ mobile/lib/screens/home/home_screen.dart
@@ -27,31 +27,90 @@ class _HomeScreenState extends State<HomeScreen> {
       );
 
   Widget _buildBody() => SingleChildScrollView(
-        child: Wrap(
-          direction: Axis.vertical,
+        child: Column(
           children: _buildRows(),
         ),
       );
 
   List<Widget> _buildRows() => [
-        _buildProgressBar(),
+        _buildProgressBar(DateTime.now()),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Row(
+            children: [
+              _buildPieCard(),
+              _buildYearSelectorCard(),
+            ],
+          ),
+        ),
       ];
 
-  Widget _buildProgressBar() => StoreConnector<AppState, DateTime>(
-        builder: (ctx, date) => Column(children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              Utils.dateToFullString(date),
-              style: const TextStyle(
-                fontWeight: FontWeight.w300,
-                fontSize: 18,
-              ),
+  Widget _buildProgressBar(DateTime date) => Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Text(
+            Utils.dateToFullString(date),
+            style: const TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 18,
             ),
           ),
-          ProgressBar(date: date),
-          const SizedBox(height: 16.0),
-        ]),
-        converter: (store) => store.state.date,
+        ),
+        ProgressBar(date: date),
+        const SizedBox(height: 16.0),
+      ]);
+
+  Widget _buildPieCard() => const SizedBox(
+        width: 120,
+        height: 120,
+        child: Card(),
+      );
+
+  Widget _buildYearSelectorCard() => Flexible(
+        child: SizedBox(
+          height: 120,
+          child: Card(
+            child: StoreConnector<AppState, DateTime>(
+              builder: (ctx, date) => SizedBox(
+                width: double.infinity,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildArrowButton(
+                        () => StoreProvider.of<AppState>(context)
+                            .dispatch(const AddDateYearAction(years: -1)),
+                        false),
+                    Text(
+                      date.year.toString(),
+                      style: const TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    _buildArrowButton(
+                        () => StoreProvider.of<AppState>(context)
+                            .dispatch(const AddDateYearAction(years: 1)),
+                        true),
+                  ],
+                ),
+              ),
+              converter: (store) => store.state.date,
+            ),
+          ),
+        ),
+      );
+
+  Widget _buildArrowButton(Function() onPressed, bool right) => TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.zero,
+          minimumSize: Size.zero,
+        ),
+        child: Icon(
+          right ? Icons.arrow_right : Icons.arrow_left,
+          size: 40,
+          color: Colors.grey,
+        ),
       );
 }
