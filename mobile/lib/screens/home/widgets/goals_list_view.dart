42b225913b8ea280786b563343287cfc66bbce28
Nico Villar <nvillar@marppa.com>
2023-12-24 21:15:52 -0300

    Add goal deletion dialog just for testing

--- mobile/lib/screens/home/widgets/goals_list_view.dart
+++ mobile/lib/screens/home/widgets/goals_list_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:mobile/models/index.dart';
+import 'package:mobile/store/actions.dart';
+import 'package:mobile/store/app_state.dart';
+import 'package:mobile/store/local_storage.dart';
+import 'package:redux/redux.dart';
 
 class GoalsListView extends StatefulWidget {
   final List<Goal> goals;
@@ -22,12 +27,44 @@ class _GoalsListViewState extends State<GoalsListView> {
       itemBuilder: (ctx, idx) => ListTile(
         onTap: () => Navigator.of(ctx)
             .pushNamed('/goal', arguments: widget.goals[idx].goalID),
+        onLongPress: () => _showDeleteDialog(ctx, widget.goals[idx]),
         title: Text(
           widget.goals[idx].title,
           style: const TextStyle(
             fontSize: 14,
           ),
         ),
+      ),
+    );
+  }
+
+  void _showDeleteDialog(BuildContext context, Goal goal) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Delete goal'),
+        content: Text('Are you sure you want to delete \'${goal.title}\'?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text(
+              'Cancel',
+              style: TextStyle(color: Color(0xff606060)),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Store<AppState> store = StoreProvider.of<AppState>(context);
+              store.dispatch(DeleteGoalAction(goal: goal));
+              LocalStorage.saveGoals(store.state.goals);
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              'Delete',
+              style: TextStyle(color: Colors.redAccent),
+            ),
+          ),
+        ],
       ),
     );
   }
