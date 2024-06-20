6c4e1e455d17db8a8f3e8b682d1f129384ee160a
nicoovillarr <nvillar@marppa.com>
2024-06-20 14:51:48 -0300

    Vercel deploy configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,4 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(require('./routes'))
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log('App running on port 3000!')
-)
+module.exports = app
