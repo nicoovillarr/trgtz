3052107acbb31ad9e802f93ae730fcbf51148823
nicoovillarr <nvillar@marppa.com>
2024-06-20 14:59:44 -0300

    Add default api response

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to the Goals API'
+  })
+})
 app.use('/goals', require('./goal.route'))
 app.use('/auth', require('./auth.route'))
 app.use('/users', require('./user.route'))
