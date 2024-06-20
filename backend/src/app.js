36b330b7105858579dfedb181024c710caabe7dc
nicoovillarr <nvillar@marppa.com>
2024-06-20 15:12:25 -0300

    Compress all responses

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const connectDB = require('./config/database')
 connectDB()
 
+const compression = require('compression')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(compression())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
