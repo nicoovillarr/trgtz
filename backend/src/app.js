0e91a51641c63d24f1ca32b679b4c1fd56adaa48
nicoovillarr <nvillar@marppa.com>
2024-06-20 14:55:33 -0300

    Add /api to all routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,6 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(require('./routes'))
+app.use('/api', require('./routes'))
 
 module.exports = app
