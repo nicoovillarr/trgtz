6d7cf3707800f7d21de570c2ceeda11e7dd709be
nicoovillarr <nvillar@marppa.com>
2024-06-21 13:42:40 -0300

    wip

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,6 @@ app.use(compression())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use('/', require('./routes'))
+app.use('/api', require('./routes'))
 
 module.exports = app
