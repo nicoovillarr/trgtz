77cb366ef2ed6d6fad9b264f69083db5a8963d51
nicoovillarr <nvillar@marppa.com>
2024-06-21 13:06:03 -0300

    Remove the /api suffix

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,6 @@ app.use(compression())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use('/api', require('./routes'))
+app.use('/', require('./routes'))
 
 module.exports = app
