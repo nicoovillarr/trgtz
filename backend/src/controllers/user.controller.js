606da676317aee6fe42ac62cb76dde8aa93755e4
nicoovillarr <nvillar@marppa.com>
2024-07-01 13:10:45 -0300

    #30: Request/answer friendship from backend

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,7 +3,6 @@ const userService = require('../services/user.service')
 const getMe = async (req, res) => {
   try {
     const user = (await userService.getUserInfo(req.user)).toJSON()
-    delete user.sessions
     res.status(200).json(user)
   } catch (error) {
     res.status(500).json(error)
@@ -23,7 +22,60 @@ const patchUser = async (req, res) => {
   }
 }
 
+const sendFriendRequest = async (req, res) => {
+  try {
+    const _id = req.user
+    const { recipientId } = req.body
+    if (!userService.userExist(recipientId)) {
+      res.status(400).json({ message: 'Recipient not found.' })
+      return
+    }
+
+    if (!(await userService.canSendFriendRequest(_id, recipientId))) {
+      res
+        .status(405)
+        .json({ message: 'You are not allowed to send a friend request now.' })
+      return
+    }
+
+    await userService.sendFriendRequest(_id, recipientId)
+
+    res.status(204).end()
+  } catch (error) {
+    res.status(500).json(error)
+    console.error('Error sending friend request: ', error)
+  }
+}
+
+const answerFriendRequest = async (req, res) => {
+  try {
+    const _id = req.user
+    const { requesterId, answer } = req.body
+    if (!(await userService.userExist(requesterId))) {
+      res.status(400).json({ message: 'Requester not found.' })
+      return
+    }
+
+    const answered = await userService.answerFriendRequest(
+      _id,
+      requesterId,
+      answer
+    )
+    if (!answered) {
+      res.status(400).json({ message: 'Friend request not found.' })
+      return
+    }
+
+    res.status(204).end()
+  } catch (error) {
+    res.status(500).json(error)
+    console.error('Error answering friend request: ', error)
+  }
+}
+
 module.exports = {
   getMe,
-  patchUser
+  patchUser,
+  sendFriendRequest,
+  answerFriendRequest
 }
