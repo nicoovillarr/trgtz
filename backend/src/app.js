5718f22efd5eb27ccdf851b6e545eb42999f6d1c
nicoovillarr <nvillar@marppa.com>
2024-07-17 21:57:18 -0300

    #36: Send friends notification

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const connectDB = require('./config/database')
 connectDB()
 
@@ -13,5 +15,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(info)
 app.use('/', require('./routes'))
+
+const admin = require('firebase-admin')
+const serviceAccount = require('./config/firebase-admin.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
 
 module.exports = app
