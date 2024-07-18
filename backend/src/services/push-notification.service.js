416e2c71fe3c7e9ce2a970116b6eca13757472e9
nicoovillarr <nvillar@marppa.com>
2024-07-18 16:14:42 -0300

    #36: Add goal related notifications and make some improvements

--- backend/src/services/push-notification.service.js
+++ backend/src/services/push-notification.service.js
@@ -1,13 +1,26 @@
 const admin = require('firebase-admin')
+const userService = require('./user.service')
 
-const sendNotification = async (tokens, title, body) => {
+const Session = require('../models/session.model')
+
+const sendNotification = async (userId, tokens, title, body) => {
   try {
     console.log(
       `Sending notification to: ${tokens
         .map((t) => t.substring(0, 8) + '...')
         .join(', ')}`
     )
-    await admin.messaging().sendEachForMulticast({
+
+    const { firstName } = (await userService.getUserInfo(userId)).toJSON()
+    if (title.includes('$name')) {
+      title = title.replace('$name', firstName)
+    }
+
+    if (body.includes('$name')) {
+      body = body.replace('$name', firstName)
+    }
+
+    const response = await admin.messaging().sendEachForMulticast({
       tokens,
       notification: {
         title: title,
@@ -19,6 +32,13 @@ const sendNotification = async (tokens, title, body) => {
   }
 }
 
+const sendNotificationToFriends = async (userId, title, body) => {
+  const friends = await userService.getFriends(userId)
+  const tokens = await userService.getUserFirebaseTokens(friends)
+  await sendNotification(userId, tokens, title, body)
+}
+
 module.exports = {
-  sendNotification
+  sendNotification,
+  sendNotificationToFriends
 }
