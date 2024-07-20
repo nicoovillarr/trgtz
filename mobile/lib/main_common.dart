520d1c5affbbf34ed305ee92740dd9dfbde29f89
nicoovillarr <nvillar@marppa.com>
2024-07-20 19:39:24 -0300

    #54: Create project environments

--- mobile/lib/main_common.dart
+++ mobile/lib/main_common.dart
@@ -5,20 +5,15 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:trgtz/constants.dart';
+import 'package:trgtz/app.dart';
 import 'package:trgtz/core/exceptions/index.dart';
 import 'package:trgtz/core/index.dart';
-import 'package:trgtz/firebase_options.dart';
-import 'package:trgtz/screens/auth/index.dart';
-import 'package:trgtz/screens/friends/index.dart';
-import 'package:trgtz/screens/goal/index.dart';
-import 'package:trgtz/screens/index.dart';
 import 'package:trgtz/security.dart';
 import 'package:trgtz/services/index.dart';
 import 'package:trgtz/store/index.dart';
 import 'package:redux/redux.dart';
 
-final navigator = GlobalKey<NavigatorState>();
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void showErrorDialog(GlobalKey<NavigatorState> navigator, Object error) {
   BuildContext context = navigator.currentContext!;
@@ -30,9 +25,11 @@ void showErrorDialog(GlobalKey<NavigatorState> navigator, Object error) {
           ErrorDialog(innerException: error is AppException ? error : null)));
 }
 
-void main() async {
+void mainCommon(
+    {required String flavor, required FirebaseOptions options}) async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: '.$flavor.env');
+  await Firebase.initializeApp(options: options);
   await FirebaseHelperService.init();
   AppState initialState = AppState(
     date: DateTime.now(),
@@ -57,61 +54,18 @@ void main() async {
 
   FlutterError.onError = (errorDetails) {
     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-    showErrorDialog(navigator, errorDetails.exception);
+    showErrorDialog(navigatorKey, errorDetails.exception);
   };
 
   PlatformDispatcher.instance.onError = (error, stack) {
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    showErrorDialog(navigator, error);
+    showErrorDialog(navigatorKey, error);
     return true;
   };
 
   runApp(MyApp(
-    navigatorKey: navigator,
+    flavor: 'flavor',
     initialState: initialState,
     initialRoute: loggedIn ? '/home' : '/login',
   ));
-}
-
-class MyApp extends StatelessWidget {
-  final AppState initialState;
-  final String initialRoute;
-  final GlobalKey<NavigatorState> navigatorKey;
-  late final Store<AppState> _store = Store<AppState>(
-    reduce,
-    initialState: initialState,
-  );
-
-  MyApp({
-    super.key,
-    required this.initialState,
-    required this.initialRoute,
-    required this.navigatorKey,
-  });
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return StoreProvider(
-      store: _store,
-      child: MaterialApp(
-        title: appName,
-        navigatorKey: navigatorKey,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        initialRoute: initialRoute,
-        routes: {
-          '/login': (context) => const LoginScreen(),
-          '/signup': (context) => const SignupScreen(),
-          '/home': (context) => const HomeScreen(),
-          '/goal': (context) => const GoalViewScreen(),
-          '/goal/milestones': (context) => const GoalMilestonesView(),
-          '/friends': (context) => const FriendsListScreen(),
-        },
-      ),
-    );
-  }
 }
