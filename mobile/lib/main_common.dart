8d5b879178af9916b35e5303147308cc3844bb83
nicoovillarr <60519222+nicoovillarr@users.noreply.github.com>
2024-09-10 22:56:19 -0300

    chore: remove sensitive info from main files

--- mobile/lib/main_common.dart
+++ mobile/lib/main_common.dart
@@ -1,7 +1,6 @@
-import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -14,7 +13,6 @@ import 'package:trgtz/security.dart';
 import 'package:trgtz/services/index.dart';
 import 'package:trgtz/store/index.dart';
 import 'package:redux/redux.dart';
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void showErrorDialog(GlobalKey<NavigatorState> navigator, Object error) {
@@ -29,13 +27,12 @@ void showErrorDialog(GlobalKey<NavigatorState> navigator, Object error) {
 
 void mainCommon({
   required String flavor,
-  required FirebaseOptions options,
 }) async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await dotenv.load(fileName: '.env.$flavor');
-  await Firebase.initializeApp(options: options);
+  await Firebase.initializeApp(options: _buildFirebaseOptions());
   await FirebaseHelperService.init();
   AppState initialState = AppState(
     date: DateTime.now(),
@@ -81,3 +78,14 @@ void mainCommon({
     ),
   );
 }
+
+FirebaseOptions _buildFirebaseOptions() => FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      iosBundleId: defaultTargetPlatform == TargetPlatform.iOS
+          ? dotenv.env['FIREBASE_IOS_BUNDLE_ID']!
+          : null,
+    );
