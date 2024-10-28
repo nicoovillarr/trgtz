32d9c5f54cfd16d7659e58bf05f8117158a4cce8
nicoovillarr <nicolasvillar00@gmail.com>
2024-10-28 19:46:35 -0300

    feat: test redirect from web

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,13 +4,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
-import vercelStatic from "@astrojs/vercel/static";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), icon()],
-  output: "static",
-  adapter: vercelStatic({
+  output: "server",
+  adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
